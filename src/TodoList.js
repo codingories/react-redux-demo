85c128bf3ac6d2077ccd68ba434ef7df585671da
朱骏杰 <ories.zhu@mochain.vip>
2020-12-02 21:15:32 +0800

    实现增加list

--- src/TodoList.js
+++ src/TodoList.js
@@ -9,10 +9,14 @@ class TodoList extends Component {
           <input
             value={this.props.inputValue} onChange={this.props.inputChange}
           />
-          <button>提交</button>
+          <button onClick={this.props.clickButton}>提交</button>
         </div>
         <ul>
-          <li>hi</li>
+          {
+            this.props.list.map((item, index)=>{
+              return (<li key={index}>{item}</li>)
+            })
+          }
         </ul>
       </div>
     );
@@ -21,7 +25,8 @@ class TodoList extends Component {
 
 const stateToProps = (state)=> {
   return {
-    inputValue: state.inputValue
+    inputValue: state.inputValue,
+    list: state.list
   }
 }
 
@@ -33,8 +38,13 @@ const dispatchToProps = (dispatch) => {
     }
     dispatch(action)
   }
+
+  const clickButton = ()=>{
+    let action = {type: 'add_item'}
+    dispatch(action)
+  }
   return {
-    inputChange
+    inputChange, clickButton
   }
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -9,6 +9,12 @@ const x = (state = defaultState, action)=>{
     newState.inputValue = action.value
     return newState
   }
+  if(action.type === 'add_item'){
+    let newState = JSON.parse(JSON.stringify(state))
+    newState.list.push(newState.inputValue)
+    newState.inputValue = ''
+    return newState
+  }
   return state
 }
 
